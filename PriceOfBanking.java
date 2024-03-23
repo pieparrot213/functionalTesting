4e6c2cb1d182b21c34e14b44064f0a62b1cfe000
Duc Phong <ducphong.forwork@gmail.com>
2024-03-23 16:56:05 +0700

    Commit Control Flow Testing

--- PriceOfBanking.java
+++ PriceOfBanking.java
@@ -3,34 +3,29 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class PriceOfBanking {
-    public static void bankingPrice(String a, int b) {
+    public static void bankingPrice(String typeOfCard, int age) {
         int price = 0;
 
-        if (a.equals("Normal")) {
+        if (typeOfCard.equals("Normal")) {
             price = 100000;
         }
-        else if (a.equals("Gold")) {
+        else if (typeOfCard.equals("Gold")) {
             price = 200000;
         }
-        else if (a.equals("Platinum")) {
+        else {
             price = 500000;
         }
 
-        if (b <= 0) {
-            System.out.println("Độ tuổi không hợp lệ!");
-        }
-        else {
-            if (b < 18) price = 0;
-            if (b > 55) {
+        if (age < 18) price = 0;
+        else if (age > 55) {
                 price = price / 2;
-            }
-            System.out.println("Số tiền quý khách phải trả là " + price + " đồng");
         }
-    }
+        System.out.println("Số tiền quý khách phải trả là " + price + " đồng");
+}
 
     public static void main(String[] args) {
         try {
-            File file = new File("testcases/decisionTable/test 7.txt");
+            File file = new File("testcases/controlFlowTesting/Test 3.txt");
             Scanner scanner = new Scanner(file);
 
             String str = scanner.next();
